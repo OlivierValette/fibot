9d4790839b7889e527f1ac3790063d5f2c8d17d6
Olivier Valette <olivier.valette@spi10.com>
2019-02-18 13:06:50 +0100

    QS Id search by ISIN

--- find_qs_id_by_isin.py
+++ find_qs_id_by_isin.py
@@ -14,16 +14,14 @@ def find_qs_id_by_isin(id):
     pageresponse = requests.get(target, timeout=5)
     if pageresponse.status_code == 200:
         pagecontent = BeautifulSoup(pageresponse.content, "html.parser")
-        # seek for <a href="/fr/funds/snapshot/snapshot.aspx?id=F00000MNJW">Comgest Monde I</a>
-        # under a <td class="msDataText searchLink">
-        # TODO : what if more than one id is find?
-        # for link in pagecontent.find_all("td", "searchLink"):
-        link = pagecontent.find("td", "searchLink")
-        return link.children.__next__().get('href')[-10:]
+
+        # seek for: <input id="id-produit" type="hidden" value="70584">
+        tag = pagecontent.find('input', id='id-produit')
+        return tag['value']
     else:
         print("Le code saisi ne correspond à aucun fonds référencé par Quantalys")
         return -1
 
 
-qsid = find_qs_id_by_isin("FR0011007251")
-print(qsid)
+qsid = find_qs_id_by_isin("FR0000284689")
+print("Quantalys id :", qsid)
