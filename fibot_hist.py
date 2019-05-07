95e1798273e4db6d9ea31c080e7c1fbcbc530d79
Olivier Valette <olivier.valette@spi10.com>
2019-05-07 15:34:44 +0200

    Reserved for historical portfolio values

---
+++ fibot_hist.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import mysql.connector
+from config import db_config
+
+
+# Main function
+# fibot: update historical tables (on a monthly basis)
+def main():
+    cnx = mysql.connector.connect(**db_config)
+    cur_fdh = cnx.cursor(buffered=True, dictionary=True)
+    query_hist_values = (
+        " SELECT MAX(lvdate)"
+        " AS ldate"
+        " FROM portfolio_hist"
+        " WHERE fund_id = %s")
+    insert_hist_values = (
+        "INSERT INTO fund_hist (fund_id, lvalue, lvdate) "
+        "VALUES (%s, %s, %s)")
+
+
+if __name__ == '__main__':
+    main()
